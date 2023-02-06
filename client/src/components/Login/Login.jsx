8c6235782d7f9801046944a3f8b1a9c4806f2756
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2023-02-05 23:39:29 -0500

    Update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -42,7 +42,7 @@ export default function Login() {
     const loginSubmit = async (e) =>{
         e.preventDefault()
         try {
-            const response = await axios('https://artisup-production.up.railway.app/signin',{
+            const response = await axios('https://artisup-production.up.railway.app/auth/signin',{
                 method:'POST',
                 data:input
             })
