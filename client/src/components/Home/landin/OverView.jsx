29b73f3b03ee4c0012cf43a3c819dccc46c5c239
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2023-04-05 15:01:35 -0500

    Update OverView.jsx

--- client/src/components/Home/landin/OverView.jsx
+++ client/src/components/Home/landin/OverView.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 
 const img = 'https://res.cloudinary.com/esteban3232/image/upload/v1664933611/eventApp/marco-bicca-bpNr_FJNwrc-unsplash_ykupzo.jpg'
 
-const rol = window.localStorage.getItem('Rol')
 
 export default function OverView() {
 
@@ -12,13 +11,12 @@ export default function OverView() {
             <div className='container mx-auto flex justify-end overflow-hidden'>
                 <img src={img} alt='' className='rounded-2xl' data-aos='fade-up' data-aos-offset='400' />
             </div>
-/*
-            //<div className='flex items-center pb-10 pt-10 justify-center max-w-sm lg:max-w-full mx-auto lg:mx-0 gap-x-2 lg:gap-x-6' data-aos='fade-down'
-                //data-aos-delay='400'>
-                //<Link to={rol === 'USER'? '/user/artists' : rol === 'ARTIST'? : '/user/artists' :  '/signup'} className='text-white btn btn-md p-5 pb-5 lg:btn-lg btn-amber flex items-center justify-center lg:gap-x-4' >
-                //    {rol ?  ' Descubre Tus Artistas Favoritos' : 'Registrarme' } 
-              //  </Link>
-            //</div>*/
+            <div className='flex items-center pb-10 pt-10 justify-center max-w-sm lg:max-w-full mx-auto lg:mx-0 gap-x-2 lg:gap-x-6' data-aos='fade-down'
+                data-aos-delay='400'>
+                <Link to='/signup' className='text-white btn btn-md p-5 pb-5 lg:btn-lg btn-amber flex items-center justify-center lg:gap-x-4' >
+                  Registrarme'
+                </Link>
+            </div>
 
         </section>
     )
